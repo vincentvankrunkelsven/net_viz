7d2d88df3fb8b2eeb603f59767929d01ec6efb7d
nicksolomon <nicksolomon10@gmail.com>
2017-03-21 14:51:42 -0700

    update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,5 +1,3 @@
-pkgs <- c("igraph",
-          "surveillance",
-          "ggraph")
-
-install.packages(pkgs)
+install.packages("igraph", repos = "http://cran.rstudio.com")
+install.packages("ggraph", repos = "http://cran.rstudio.com")
+install.packages("surveillance", repos = "http://cran.rstudio.com")
